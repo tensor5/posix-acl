48fe4c1d4bb3d7f123143eef9bd90612a995c95c
Nicola Squartini <tensor5@gmail.com>
2014-10-23 14:51:52 +0900

    Use Safe Haskell
    Signed-off-by: Nicola Squartini <tensor5@gmail.com>

--- System/Posix/ACL.hs
+++ System/Posix/ACL.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE Safe                  #-}
 
 -- | Support for POSIX.1e /Access Control Lists/ (ACL), defined in
 -- section 23 of the draft standard IEEE Std 1003.1e.
